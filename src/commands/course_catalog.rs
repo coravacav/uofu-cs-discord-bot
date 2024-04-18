2e9b0dcec4406377da973f0ba2464717c689d1fc
Stefan Todorov <stefan.b.todorov@gmail.com>
2024-04-17 21:12:51 -0600

    update the course catalog command

--- src/commands/course_catalog.rs
+++ src/commands/course_catalog.rs
@@ -2,55 +2,63 @@ use crate::data::PoiseContext;
 use color_eyre::eyre::Result;
 use poise::{serenity_prelude as serenity, CreateReply};
 use serde::Deserialize;
+use std::sync::OnceLock;
+
+#[derive(Debug, Deserialize, Default)]
+struct CourseList(Vec<Course>);
 
 #[derive(Debug, Deserialize)]
-struct CatalogEntry {
-    #[serde(rename = "pid")]
-    page_id: String,
+#[serde(rename_all = "camelCase")]
+#[non_exhaustive]
+struct Course {
+    #[serde(rename = "__catalogCourseId")]
+    course_id: String,
+    pid: String,
     title: String,
-    description: String,
-    code: String,
 }
 
+// For now we are only going to fetch the data once at the start of the bot.
+static COURSES: OnceLock<CourseList> = OnceLock::new();
+
 #[poise::command(slash_command, prefix_command, rename = "catalog")]
-pub async fn course_catalog(ctx: PoiseContext<'_>, course_name: String) -> Result<()> {
+pub async fn course_catalog(ctx: PoiseContext<'_>, course_id: String) -> Result<()> {
     ctx.defer().await?;
 
-    let formatted_course_name = course_name
-        .chars()
-        .filter(|c| !c.is_whitespace())
-        .collect::<String>();
+    let courses = COURSES.get_or_init(|| {
+        reqwest::blocking::get(
+            "https://utah.kuali.co/api/v1/catalog/courses/6529bbfa1170af001cdefde1",
+        )
+        .map(|body| body.json().unwrap_or_default())
+        .unwrap_or_default()
+    });
 
-    // The catalog ID is just hard-coded for now.
-    // I'm not sure if there's a better/dynamic way to find this.
-    // See <https://developers.kuali.co/#header-hosting> for more info.
-    let catalog_id = "6529bbfa1170af001cdefde1";
-    let catalog_url = format!("https://utah.kuali.co/api/v1/catalog/search/{catalog_id}");
-
-    let client = reqwest::Client::new();
-    let response = client
-        .get(catalog_url)
-        .query(&[("q", formatted_course_name.as_str())])
-        .send()
-        .await?
-        .error_for_status()?;
-
-    let data: Vec<CatalogEntry> = response.json().await?;
-
-    if let Some(entry) = data.first() {
-        let title = format!("{} - {}", entry.code, entry.title);
-        let class_url = format!("https://catalog.utah.edu/#/courses/{}", entry.page_id);
-
-        let embed = serenity::CreateEmbed::new()
-            .title(title)
-            .description(&entry.description)
-            .url(class_url);
-        ctx.send(CreateReply::default().embed(embed).reply(true))
+    if courses.0.is_empty() {
+        ctx.reply("The course list couldn't be loaded! Let the mods know.")
             .await?;
-    } else {
-        ctx.reply("Sorry, I could not find that course! :pensive:")
-            .await?;
+        return Ok(());
     }
+
+    let Some(course) = courses
+        .0
+        .iter()
+        .find(|course| course.course_id == course_id)
+    else {
+        ctx.reply("Could not find a course with that id!").await?;
+        return Ok(());
+    };
+
+    let course_url = format!("https://catalog.utah.edu/#/courses/{}", course.pid);
+
+    ctx.send(
+        CreateReply::default()
+            .embed(
+                serenity::CreateEmbed::new()
+                    .title(format!("{} - {}", course.course_id, course.title))
+                    .url(course_url),
+            )
+            .reply(true),
+    )
+    .await?;
 
     Ok(())
 }
