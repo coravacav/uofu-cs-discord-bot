9bbdad3070f5fafe52b119e8a3a903a13c4ab875
Stefan Todorov <stefan.b.todorov@gmail.com>
2024-08-29 00:00:15 -0600

    updated lynch to have sane timeouts

--- bot-lib/src/commands/llm_prompt.rs
+++ bot-lib/src/commands/llm_prompt.rs
@@ -1,8 +1,8 @@
 use crate::data::PoiseContext;
-use color_eyre::eyre::{Context, OptionExt, Result};
+use color_eyre::eyre::{bail, Context, OptionExt, Result};
 use dashmap::{try_result::TryResult, DashMap};
 use poise::{
-    serenity_prelude::{self as serenity, UserId},
+    serenity_prelude::{self as serenity, ChannelId, UserId},
     CreateReply,
 };
 use std::{
@@ -10,39 +10,75 @@ use std::{
     time::{Duration, Instant},
 };
 
-static LAST_REQUESTED_BY_USERID: LazyLock<DashMap<UserId, Instant>> = LazyLock::new(DashMap::new);
+/// This static is used to track when you're in the bots-channel
+static INSTANT_BY_USER_ID: LazyLock<DashMap<UserId, Instant>> = LazyLock::new(DashMap::new);
+const BOTS_CHANNEL_PER_USER_TIMEOUT_SECONDS: u64 = 60;
+/// This static is used to track all non-bots-channel messages
+static INSTANT_BY_CHANNEL_ID: LazyLock<DashMap<ChannelId, Instant>> = LazyLock::new(DashMap::new);
+const PER_CHANNEL_TIMEOUT_SECONDS: u64 = 600;
 
-#[poise::command(
-    slash_command,
-    prefix_command,
-    rename = "llm",
-    description_localized("en-US", "Ask kingfisher anything!")
-)]
-pub async fn llm_prompt(ctx: PoiseContext<'_>, prompt: String) -> Result<()> {
-    let prompt = Arc::new(prompt);
+const BOTS_CHANNEL_CHANNEL_ID: ChannelId = ChannelId::new(1105683912715415652);
+
+async fn try_lock_llm(ctx: PoiseContext<'_>) -> Result<()> {
+    let channel_id = ctx.channel_id();
     let user_id = ctx.author().id;
 
-    match LAST_REQUESTED_BY_USERID.try_get(&user_id) {
-        TryResult::Locked => {
-            let reply = CreateReply::default()
-                .ephemeral(true)
-                .content("Are you spamming?");
-            ctx.send(reply).await?;
-            return Ok(());
-        }
-        TryResult::Present(last_time) => {
-            if last_time.elapsed() < Duration::from_secs(60) {
+    if channel_id == BOTS_CHANNEL_CHANNEL_ID {
+        match INSTANT_BY_USER_ID.try_get(&user_id) {
+            TryResult::Locked => {
+                let reply = CreateReply::default()
+                    .ephemeral(true)
+                    .content("Are you spamming?");
+                ctx.send(reply).await?;
+                bail!("Are you spamming?");
+            }
+            TryResult::Present(last_time)
+                if last_time.elapsed()
+                    < Duration::from_secs(BOTS_CHANNEL_PER_USER_TIMEOUT_SECONDS) =>
+            {
                 let reply = CreateReply::default()
                     .ephemeral(true)
                     .content("Please wait a minute before asking again");
                 ctx.send(reply).await?;
-                return Ok(());
+                bail!("Please wait a minute before asking again");
             }
+            _ => {}
         }
-        _ => {}
+
+        INSTANT_BY_USER_ID.insert(user_id, Instant::now());
+    } else {
+        match INSTANT_BY_CHANNEL_ID.try_get(&channel_id) {
+            TryResult::Locked => {
+                let reply = CreateReply::default()
+                    .ephemeral(true)
+                    .content("Are you spamming?");
+                ctx.send(reply).await?;
+                bail!("Are you spamming?");
+            }
+            TryResult::Present(last_time)
+                if last_time.elapsed() < Duration::from_secs(PER_CHANNEL_TIMEOUT_SECONDS) =>
+            {
+                let reply = CreateReply::default()
+                    .ephemeral(true)
+                    .content("Please wait 10 minutes before asking again");
+                ctx.send(reply).await?;
+                bail!("Please wait 10 minutes before asking again");
+            }
+            _ => {}
+        }
+
+        INSTANT_BY_CHANNEL_ID.insert(channel_id, Instant::now());
     }
 
-    LAST_REQUESTED_BY_USERID.insert(user_id, Instant::now());
+    Ok(())
+}
+
+/// Ask kingfisher anything!
+#[poise::command(slash_command, prefix_command, rename = "llm")]
+pub async fn llm_prompt(ctx: PoiseContext<'_>, prompt: String) -> Result<()> {
+    try_lock_llm(ctx).await.ok();
+    let prompt = Arc::new(prompt);
+
     ctx.defer().await?;
 
     let (reply, reply_rx) = tokio::sync::oneshot::channel();
