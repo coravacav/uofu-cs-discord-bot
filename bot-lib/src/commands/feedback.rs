e03cdc532d2e88b1b28dad5ed782c26d2ab427fe
Stefan Todorov <stefan.b.todorov@gmail.com>
2024-08-28 18:02:10 -0600

    make send feedback not ephemeral

--- bot-lib/src/commands/feedback.rs
+++ bot-lib/src/commands/feedback.rs
@@ -2,7 +2,7 @@ use crate::data::PoiseContext;
 use color_eyre::eyre::Result;
 
 /// Command to easily create a github issue for Kingfisher
-#[poise::command(slash_command, ephemeral = true)]
+#[poise::command(slash_command)]
 pub async fn send_feedback(ctx: PoiseContext<'_>) -> Result<()> {
     ctx.say("<https://github.com/coravacav/uofu-cs-discord-bot/issues/new>")
         .await?;
