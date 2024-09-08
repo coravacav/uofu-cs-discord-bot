389d52d57df8021e58404b5d793f1bcc4acfcbae
Stefan Todorov <stefan.b.todorov@gmail.com>
2024-09-08 17:49:16 -0600

    alcoholism and make it more resistant

--- bot-lib/src/lang/rule.rs
+++ bot-lib/src/lang/rule.rs
@@ -1,7 +1,10 @@
 use super::case::{parse_all_cases, Case};
-use nom::{bytes::complete::tag, multi::separated_list1, Finish};
+use nom::{
+    bytes::complete::tag, character::complete::multispace1, multi::separated_list1,
+    sequence::tuple, Finish, IResult,
+};
 
-const SPLIT_RULE_SEPARATOR: &str = "\nor\n";
+const SPLIT_RULE_SEPARATOR: &str = "\nor";
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Rule {
@@ -14,8 +17,12 @@ impl Rule {
     }
 }
 
+pub fn parse_separator(input: &str) -> IResult<&str, ()> {
+    tuple((tag(SPLIT_RULE_SEPARATOR), multispace1))(input).map(|(v, _)| (v, ()))
+}
+
 pub fn parse_rules(input: &str) -> Option<Vec<Rule>> {
-    separated_list1(tag(SPLIT_RULE_SEPARATOR), parse_all_cases)(input)
+    separated_list1(parse_separator, parse_all_cases)(input)
         .finish()
         .map(|(_, vec_vec_case)| vec_vec_case.into_iter().map(Rule::new).collect())
         .ok()
