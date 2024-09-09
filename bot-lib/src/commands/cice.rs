70b80ec73861d834a20a3077745d6736ac56dcc7
Stefan Todorov <stefan.b.todorov@gmail.com>
2024-09-08 23:05:29 -0600

    add coin side

--- bot-lib/src/commands/cice.rs
+++ bot-lib/src/commands/cice.rs
@@ -6,6 +6,22 @@ use rand::Rng;
 #[poise::command(slash_command, prefix_command, rename = "coinflip")]
 pub async fn coinflip(ctx: PoiseContext<'_>, optional_explanation: Option<String>) -> Result<()> {
     let heads = rand::thread_rng().gen_bool(0.5);
+    let lands_on_its_side = rand::thread_rng().gen_bool(0.001);
+
+    if lands_on_its_side {
+        ctx.say(format!(
+            "{} flipped a coin and it LANDED ON ITS SIDE??? (0.1% chance){}",
+            ctx.author().mention(),
+            if let Some(reason) = optional_explanation {
+                format!(" \"{}\"", reason)
+            } else {
+                String::new()
+            }
+        ))
+        .await?;
+
+        return Ok(());
+    }
 
     ctx.say_then_delete(format!(
         "{} flipped a coin and got {}{}",
