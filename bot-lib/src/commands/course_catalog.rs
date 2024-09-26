79b732212d5987af267e6ab101a070b85df8eda3
Stefan Todorov <stefan.b.todorov@gmail.com>
2024-09-25 19:41:33 -0600

    added search_catalog

--- bot-lib/src/commands/course_catalog.rs
+++ bot-lib/src/commands/course_catalog.rs
@@ -1,11 +1,13 @@
-use crate::{courses::get_course, data::PoiseContext};
+use crate::{
+    courses::{get_course, Course, COURSES},
+    data::PoiseContext,
+};
 use color_eyre::eyre::Result;
+use itertools::Itertools;
 use poise::{serenity_prelude as serenity, CreateReply};
 
-#[poise::command(slash_command, rename = "catalog")]
-pub async fn course_catalog(ctx: PoiseContext<'_>, course_id: String) -> Result<()> {
-    ctx.defer().await?;
-
+#[poise::command(slash_command)]
+pub async fn catalog(ctx: PoiseContext<'_>, course_id: String) -> Result<()> {
     let mut course_id = course_id
         .to_uppercase()
         .chars()
@@ -27,29 +29,91 @@ pub async fn course_catalog(ctx: PoiseContext<'_>, course_id: String) -> Result<
         return Ok(());
     };
 
-    ctx.send(
-        CreateReply::default()
-            .embed(
-                serenity::CreateEmbed::new()
-                    .title(format!(
-                        "{} - {}{}",
-                        course_id,
-                        course.long_name.clone(),
-                        if course.are_there_duplicates {
-                            " Note: maybe a duplicate, the U has bad APIs"
-                        } else {
-                            ""
-                        }
-                    ))
-                    .description(course.description.clone())
-                    .url(format!(
-                        "https://catalog.utah.edu/courses/{}",
-                        course.course_group_id
-                    )),
-            )
-            .reply(true),
-    )
-    .await?;
+    ctx.send(get_course_reply(&course)).await?;
+
+    Ok(())
+}
+
+fn get_course_reply(course: &Course) -> CreateReply {
+    CreateReply::default()
+        .embed(
+            serenity::CreateEmbed::new()
+                .title(format!(
+                    "{} - {}{}",
+                    course.course_id,
+                    course.long_name.clone(),
+                    if course.are_there_duplicates {
+                        " Note: maybe a duplicate, the U has bad APIs"
+                    } else {
+                        ""
+                    }
+                ))
+                .description(course.description.clone())
+                .url(format!(
+                    "https://catalog.utah.edu/courses/{}",
+                    course.course_group_id
+                )),
+        )
+        .reply(true)
+}
+
+/// Searches the U of U course catalog based on a search string
+///
+/// Searches the course code, title, and description
+#[poise::command(slash_command)]
+pub async fn search_catalog(ctx: PoiseContext<'_>, search_string: String) -> Result<()> {
+    if search_string.is_empty() {
+        ctx.reply("Please provide a valid search string").await?;
+        return Ok(());
+    }
+
+    let upper_search_string = search_string.to_uppercase();
+
+    let reply = {
+        let courses = COURSES.read();
+
+        let courses = courses
+            .values()
+            .filter(|course| {
+                [
+                    course.course_id.as_str(),
+                    course.long_name.as_str(),
+                    course.description.as_str(),
+                ]
+                .iter()
+                .any(|string_to_search| {
+                    string_to_search
+                        .to_uppercase()
+                        .contains(&upper_search_string)
+                })
+            })
+            .take(20)
+            .collect_vec();
+
+        let course_count = courses.len();
+
+        match course_count {
+            0 => CreateReply::default()
+                .content(format!("No courses found for \"{}\"", search_string))
+                .reply(true)
+                .ephemeral(true),
+            1 => get_course_reply(courses.first().unwrap()),
+            _ => CreateReply::default()
+                .content(format!(
+                    "Found (at least) {} courses for \"{}\"\n{}",
+                    course_count,
+                    search_string,
+                    courses
+                        .iter()
+                        .map(|course| format!("`{}`", course.course_id))
+                        .join(" ")
+                ))
+                .reply(true)
+                .ephemeral(true),
+        }
+    };
+
+    ctx.send(reply).await?;
 
     Ok(())
 }
--- bot-lib/src/courses.rs
+++ bot-lib/src/courses.rs
@@ -1,22 +1,33 @@
 use parking_lot::RwLock;
 use serde::Deserialize;
-use std::{collections::HashMap, sync::LazyLock};
+use std::{
+    collections::HashMap,
+    sync::{Arc, LazyLock},
+};
 
-static COURSES: LazyLock<RwLock<HashMap<String, Course>>> = LazyLock::new(|| {
+pub(crate) static COURSES: LazyLock<RwLock<HashMap<String, Course>>> = LazyLock::new(|| {
+    let instant = std::time::Instant::now();
     let file = std::fs::File::open("classes.json").unwrap();
     let file_reader = std::io::BufReader::new(file);
     let file: File = serde_json::from_reader(file_reader).unwrap();
 
-    let mut courses = HashMap::new();
+    let mut courses: HashMap<String, Course> = HashMap::new();
 
     for mut course in file.data {
-        let current = courses.get(&course.course_id);
+        let current = courses.get(&*course.course_id);
         let are_there_duplicates = current.is_some();
         course.are_there_duplicates = are_there_duplicates;
-        courses.insert(course.course_id.clone(), course);
+        courses.insert((*course.course_id).clone(), course);
     }
 
-    tracing::info!("Loaded {} courses", courses.len());
+    let elapsed = instant.elapsed();
+
+    tracing::info!(
+        "Loaded {} courses in {}.{} seconds",
+        courses.len(),
+        elapsed.as_secs(),
+        elapsed.subsec_millis()
+    );
 
     courses.into()
 });
@@ -36,11 +47,16 @@ struct File {
 #[serde(rename_all = "camelCase")]
 #[non_exhaustive]
 pub struct Course {
+    /// The course code, eg. CS2420
     #[serde(rename = "code")]
-    pub course_id: String,
+    pub course_id: Arc<String>,
+    /// The long name of the course, eg. Introduction to Computer Science
     pub long_name: String,
+    /// The description of the course
     pub description: String,
+    /// Some arbitrary number the U gave
     pub course_group_id: String,
+    /// Whether or not there are duplicates. The U has a bad API.
     #[serde(skip)]
     pub are_there_duplicates: bool,
 }
