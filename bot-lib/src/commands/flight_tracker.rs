c897916f9c9fc6a219a0be752083adcbd2bd33d2
julestheshiba <julestheshiba@gmail.com>
2025-11-12 16:52:20 -0700

    flight tracker regex check

---
+++ bot-lib/src/commands/flight_tracker.rs
@@ -0,0 +1,24 @@
+use poise::{CreateReply, serenity_prelude as serenity};
+use regex::Regex;
+
+#[poise::command(slash_command, rename = "track flight")]
+pub async fn track_flight(
+    ctx: PoiseContext<'_>,
+    search: String,
+) -> Result<()> {
+    ctx.defer().await?;
+
+    let search: String = search
+        .chars()
+        .filter(|c| c.is_alphanumeric())
+        .collect::<String>();
+
+    let re = Regex::new(r"[A-Za-z]{2,3}[0-9]{1,4}").unwrap();
+
+    if search.len() < 2 || search.is_empty() ||  re.is_match(search) {
+        ctx.reply("Please provide a valid  flight number").await?;
+        return Ok(());
+    }
+
+
+}
