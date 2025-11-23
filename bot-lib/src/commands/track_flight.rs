8b2fd11c939e2d7531eb6b17839bdec314fdd8ea
julestheshiba <julestheshiba@gmail.com>
2025-11-23 00:13:58 -0700

    more flight tracker

--- bot-lib/src/commands/track_flight.rs
+++ bot-lib/src/commands/track_flight.rs
@@ -1,12 +1,14 @@
+use crate::data::PoiseContext;
+use color_eyre::eyre::Result;
 use poise::{CreateReply, serenity_prelude as serenity};
 use regex::Regex;
 
-const BASE_FLIGHTAWARE_URL: &str = "https://www.flightaware.com/";
 
 ///get information on a specified flight
-#[poise::command(slash_command, rename = "track flight")]
+#[poise::command(slash_command, rename = "trackflight")]
 pub async fn track_flight(
     ctx: PoiseContext<'_>,
+
     search: String,
 ) -> Result<()> {
     ctx.defer().await?;
@@ -18,10 +20,10 @@ pub async fn track_flight(
 
     let re = Regex::new(r"[A-Za-z]{2,3}[0-9]{1,4}").unwrap();
 
-    if search.len() < 2 || search.is_empty() ||  re.is_match(search) {
-        ctx.reply("Please provide a valid  flight number").await?;
+    if search.len() < 2 || search.is_empty() || !re.is_match(&search) {
+        ctx.reply("Please provide a valid flight number").await?;
         return Ok(());
     }
 
-
+    Ok(())
 }
