d03f6f37b3cb6d8b64f5c3534c466f073854f5ec
Stefan Todorov <stefan.b.todorov@gmail.com>
2026-07-26 04:47:34 -0500

    Upgrade nom to 8

--- bot-lib/src/lang/case.rs
+++ bot-lib/src/lang/case.rs
@@ -1,6 +1,6 @@
 use nom::{
-    IResult, bytes::complete::tag, bytes::complete::take_till, character::complete::newline,
-    combinator::opt, multi::separated_list1,
+    IResult, Parser, bytes::complete::tag, bytes::complete::take_till,
+    character::complete::newline, combinator::opt, multi::separated_list1,
 };
 
 #[derive(Clone, PartialEq, Eq)]
@@ -10,7 +10,7 @@ pub struct Case<'a> {
 }
 
 fn parse_line(input: &'_ str) -> IResult<&'_ str, Case<'_>> {
-    let (input, is_negated) = opt(tag("!"))(input)?;
+    let (input, is_negated) = opt(tag("!")).parse(input)?;
     let is_negated = is_negated.is_some();
     let (input, _) = tag("r ")(input)?;
     let (input, unparsed_regex) = take_till(|c| c == '\n')(input)?;
@@ -28,5 +28,5 @@ fn parse_case(input: &'_ str) -> IResult<&'_ str, Case<'_>> {
 }
 
 pub fn parse_all_cases(input: &'_ str) -> IResult<&'_ str, Vec<Case<'_>>> {
-    separated_list1(newline, parse_case)(input)
+    separated_list1(newline, parse_case).parse(input)
 }
--- bot-lib/src/lang/rule.rs
+++ bot-lib/src/lang/rule.rs
@@ -1,8 +1,8 @@
 use super::case::{Case, parse_all_cases};
 use bot_traits::ForwardRefToTracing;
 use nom::{
-    Finish, IResult, bytes::complete::tag, character::complete::multispace1,
-    multi::separated_list1, sequence::tuple,
+    Finish, IResult, Parser, bytes::complete::tag, character::complete::multispace1,
+    multi::separated_list1,
 };
 
 /// A rule is a single case of success for a given ruleset
@@ -19,11 +19,14 @@ impl<'a> Rule<'a> {
 }
 
 pub fn parse_separator(input: &str) -> IResult<&str, ()> {
-    tuple((tag("\nor"), multispace1))(input).map(|(v, _)| (v, ()))
+    (tag("\nor"), multispace1)
+        .parse(input)
+        .map(|(v, _)| (v, ()))
 }
 
 pub fn parse_rules(input: &str) -> Option<Vec<Rule<'_>>> {
-    separated_list1(parse_separator, parse_all_cases)(input)
+    separated_list1(parse_separator, parse_all_cases)
+        .parse(input)
         .finish()
         .map(|(_, vec_vec_case)| vec_vec_case.into_iter().map(Rule::new).collect())
         .trace_err_ok()
